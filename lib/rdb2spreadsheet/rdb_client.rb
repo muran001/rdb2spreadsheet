6344bfe6ebd524790bdc55a0b7541d33b16fa171
Masatoshi Murakami <masatoshimurakami@10005664.local>
2016-06-01 10:24:53 +0900

    version up

--- lib/rdb2spreadsheet/rdb_client.rb
+++ lib/rdb2spreadsheet/rdb_client.rb
@@ -4,7 +4,7 @@ require 'yaml'
 module Rdb2spreadsheet
   class RdbClient
     def initialize(configs, zone_default = 'Tokyo')
-      Time.zone_default =  Time.find_zone! zone_default # config.time_zone
+      Time.zone_default = zone_default # config.time_zone
       ActiveRecord::Base.default_timezone = :local # config.active_record.default_timezone
       ActiveRecord::Base.establish_connection(configs)
       @connection = ActiveRecord::Base.connection
